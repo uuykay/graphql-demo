0388d692b986d2014bbc714ca6b4ef0617369108
Will <william.y.kuang@gmail.com>
2019-07-11 23:15:16 +1000

    first GraphQL server with typedefs and resolvers

--- src/index.js
+++ src/index.js
@@ -3,8 +3,36 @@ import { GraphQLServer } from "graphql-yoga";
 // Type definitions (schema)
 const typeDefs = `
 	type Query{
-		hello: String
+		hello: String,
+		name: String,
+		location: String,
+		bio: String
 	}
 `;
 
 // Resolvers
+const resolvers = {
+  Query: {
+    hello() {
+      return "This is my first query!";
+    },
+    name() {
+      return "William Kuang";
+    },
+    location() {
+      return "Sydney";
+    },
+    bio() {
+      return "An enthusiastic JS developer";
+    }
+  }
+};
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers
+});
+
+server.start(() => {
+  console.log("The server is up");
+});
