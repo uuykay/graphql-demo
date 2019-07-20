cc74235192d0c28d6390ea0e43ba1067e5e5f26d
Will <william.y.kuang@gmail.com>
2019-07-20 17:06:22 +1000

    pass in operation argument

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { GraphQLServer } from "graphql-yoga";
 // Type definitions (schema)
 const typeDefs = `
 	type Query{
+		greeting(name: String!): String!
 		me: User!
 		post: Post!
 	}
@@ -26,6 +27,9 @@ const typeDefs = `
 // Resolvers
 const resolvers = {
   Query: {
+    greeting(parent, args, ctx, info) {
+      return `Hello ${args.name}`;
+    },
     me() {
       return {
         id: "abc123",
