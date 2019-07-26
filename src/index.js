7988958f1be99cd813fd13f34b378c040e11321a
Will <william.y.kuang@gmail.com>
2019-07-26 22:53:52 +1000

    Delete old query and resolvers

--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@ import { GraphQLServer } from "graphql-yoga";
 // Type definitions (schema)
 const typeDefs = `
 	type Query{
-		greeting(name: String!): String!
-		add(numbers: [Float!]!): Float!
-		grades: [Int!]!
 		me: User!
 		post: Post!
 	}
@@ -29,21 +26,6 @@ const typeDefs = `
 // Resolvers
 const resolvers = {
   Query: {
-    greeting(parent, args, ctx, info) {
-      return `Hello ${args.name}`;
-    },
-    add(parent, args, ctx, info) {
-      if (args.numbers.length === 0) {
-        return 0;
-      }
-
-      return args.numbers.reduce((accumulator, currentValue) => {
-        return accumulator + currentValue;
-      });
-    },
-    grades(parent, args, ctx, info) {
-      return [99, 80, 93];
-    },
     me() {
       return {
         id: "abc123",
