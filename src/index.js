bf02bb596eedd59c30cdfc3e2f420ed1a9f6357e
Will <william.y.kuang@gmail.com>
2019-07-26 22:35:03 +1000

    explore arrays of scalar types

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import { GraphQLServer } from "graphql-yoga";
 const typeDefs = `
 	type Query{
 		greeting(name: String!): String!
-		add(a: Float!, b: Float!): Float!
+		add(numbers: [Float!]!): Float!
+		grades: [Int!]!
 		me: User!
 		post: Post!
 	}
@@ -32,7 +33,16 @@ const resolvers = {
       return `Hello ${args.name}`;
     },
     add(parent, args, ctx, info) {
-      return args.a + args.b;
+      if (args.numbers.length === 0) {
+        return 0;
+      }
+
+      return args.numbers.reduce((accumulator, currentValue) => {
+        return accumulator + currentValue;
+      });
+    },
+    grades(parent, args, ctx, info) {
+      return [99, 80, 93];
     },
     me() {
       return {
